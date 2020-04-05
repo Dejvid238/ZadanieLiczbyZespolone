57ea729241bb25c4e57b9c32c2454f565279bf35
Dejvid238 <dawidkrekora@gmail.com>
2020-04-05 05:02:29 +0200

    plik main programu

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,25 @@
 #include <iostream>
 #include <cassert>
 
-//deklaracja przestrzeni ktore beda uzyte w programie
+//deklaracja przestrzeni nazw ktore beda uzyte w programie
+
 using std::string;
 using std::endl;
 using std::cout;
 using std::cin;
 
+//deklaracja zmiennych pomocniczych programu:
+
+LZespolona Wynik;                       //dla wartosci wyrazenia arytmetycznego
+Statystyki Zlicz = {0,0};               //dla zliczania statystyk
+WyrazenieZesp WyrZ_PytanieTestowe;      //dla okreslenia wyrazenia ktore trzeba obliczyc
+BazaTestu   BazaT = { nullptr, 0, 0 };  //dla inicjacji struktury aby nie byla pustym wskaznikiem
+
 int main(int argc, char **argv)
 {
+
+//warunek sprawdzajacy czy wybrano wersje testu, za mala liczba argumentow skutkuje odpowiednim komunikatem
+
   if (argc < 2) {
     cout << endl;
     cout << " Brak opcji okreslajacej rodzaj testu." << endl;
@@ -19,7 +30,7 @@ int main(int argc, char **argv)
     return 1;
   }
 
-BazaTestu   BazaT = { nullptr, 0, 0 };
+//warunek sprawdzjacy poprawnosc wpisanego argumentu z dopuszalnymi nazwami
 
   if (InicjalizujTest(&BazaT,argv[1]) == true) 
     {
@@ -32,22 +43,49 @@ BazaTestu   BazaT = { nullptr, 0, 0 };
       exit(0);
     }
 
-
-
-WyrazenieZesp   WyrZ_PytanieTestowe;
-
+//inicjacja petli ktora wczytuje kolejne wyrazenia dopoki jest taka mozliwosc
+//po udzieleniu prawidlowej odpowiedzi (lub 3 bledynych) wyswietlone zostaje kolejne pytanie
+//zmienna prowadzaca statystki w miedzyczasie zlicza wynik
 
 while (PobierzNastpnePytanie(&BazaT,&WyrZ_PytanieTestowe)) 
 {
   cout << "Podaj wynik operacji: ";
   Wyswietl(WyrZ_PytanieTestowe);
-  cout<<endl;
-}
-
-
+  cout<<" = "<<endl;
+  Wynik=Oblicz(WyrZ_PytanieTestowe);
+  for (int i=0;i<3;)
+    { 
+      if(Wczytaj(Wynik)==true)
+      {
+        cout<<"Odpowiedz poprawna"<<endl<<endl;
+        Zlicz.dobre++;
+        break;
+      }
+      else        
+      { 
+        if (i<2)
+      {
+        cout<<"Sprobuj jeszcze raz."<<endl<<endl;
+        i++;
+      }
+        else
+        {
+          cout<<" Prawidlowym wynikiem jest: ";
+          Wyswietl(Wynik);
+          cout<<endl<<endl;
+          Zlicz.zle++;
+          i++;
+        }
+      }
+    }      
+  }
   
+  //uruchomienie funkcji zliczajacej ilosc poprawnych i niepoprawnych odpowiedzi
+
+  Inicjuj(Zlicz);
   cout << endl;
   cout << " Koniec testu" << endl;
   cout << endl;
 
+return 0;
 }
